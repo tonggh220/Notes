bdd7951db43c88e1f4e8c885eba2a17675efbde4
guanyusheng <hwt220@outlook.com>
2021-05-04 20:20:28 +0800

    modify manjaro_setup.sh

--- conffiles/manjaro_setup.sh
+++ conffiles/manjaro_setup.sh
@@ -8,9 +8,9 @@ alias egrep='egrep --colour=auto'
 alias fgrep='fgrep --colour=auto'
 EOF
 source .zshrc
-sudo pacman -S --noconfirm gvim neovim ibus-rime deepin-screenshot appimagelauncher doublecmd-gtk2 git python-pip curl wget python2-pip min brave virt-manager qemu libvirt  ebtables dnsmasq bridge-utils openbsd-netcat
+sudo pacman -S --noconfirm gvim ibus-rime vlc minder appimagelauncher doublecmd-qt5 git python-pip curl wget min brave virt-manager qemu libvirt  ebtables dnsmasq bridge-utils openbsd-netcat remmina audacious
 setpxy
-sudo pamac install --no-confirm syncthing-gtk-python3 xdman wechat-uos linuxqq minder-git visual-studio-code-bin utools wps-office-cn wps-office-mime-cn wps-office-mui-zh-cn ttf-wps-fonts skypeforlinux-stable-bin baidunetdisk-bin typora libguestfs
+sudo pamac install --no-confirm xdman wechat-uos linuxqq visual-studio-code-bin utools wps-office-cn wps-office-mime-cn wps-office-mui-zh-cn ttf-wps-fonts skypeforlinux-stable-bin baidunetdisk-bin typora libguestfs joplin-appimage lx-music-desktop-bin
 sed -i '/unix_sock_group/s/^#//' /etc/libvirt/libvirtd.conf
 sed -i '/unix_sock_rw_perms/s/^#//' /etc/libvirt/libvirtd.conf
 sudo usermod -a -G libvirt $(whoami)
@@ -18,4 +18,5 @@ sudo modprobe -r kvm_intel
 sudo modprobe kvm_intel nested=1
 sudo systemctl enable libvirtd --now
 pip3 install --user flake8 jedi yapf isort autopep8 ansible==2.7.18
-curl -sLf https://spacevim.org/cn/install.sh | bash
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://gitee.com/bigCake/vim-plug/blob/master/plug.vim
+curl -fLo ~/.vimrc https://gitee.com/guanyusheng/Notes/blob/master/conffiles/.vimrc
